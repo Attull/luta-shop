f3cdb5643bb5e06e06ef007f33f09612090d783a
Atul Sharma <atul852798@gmail.com>
2026-06-27 11:40:05 +0530

    product API integratiion

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./style.css"
 
 function Products() {
+
+    let [products, setProducts] = useState([])
+    async function fetchData(){
+         const res = await fetch("https://fakestoreapi.com/products")
+         const data = await res.json()
+         setProducts(data)
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
   return (
-    <div>Products</div>
+    <div>
+        <h1>Product page</h1>
+        {
+            products.map((prod)=>{
+                return (
+                    <div>
+                        <img src={prod.image} height={100}/>
+                        <h5>{prod.title}</h5>
+                    </div>
+                )
+            })
+        }
+    </div>
   )
 }
 
